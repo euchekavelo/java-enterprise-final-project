455b88cd2b656d4c6c2805579dd39658decc36d9
Kirill <euchekavelo@gmail.com>
2023-05-06 02:31:02 +0300

    The remaining microservices have been added.

---
+++ inventory-service/src/main/java/ru/skillbox/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
