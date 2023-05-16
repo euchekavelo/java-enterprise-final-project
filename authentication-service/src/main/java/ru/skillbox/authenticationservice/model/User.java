159f5e41e5152158980dd49a70748273c6d1cbbf
Kirill <euchekavelo@gmail.com>
2023-05-16 22:40:06 +0300

    JSON Web Token dependency added to "authentication" microservice.

---
+++ authentication-service/src/main/java/ru/skillbox/authenticationservice/model/User.java
@@ -0,0 +1,12 @@
+package ru.skillbox.authenticationservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}
